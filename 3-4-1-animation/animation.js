6f23dd9d4269d58093ed062f2796fce576faf8f7
athena <athena0304@gmail.com>
2016-01-29 12:02:07 +0800

    modify

--- 3-4-1-animation/animation.js
+++ 3-4-1-animation/animation.js
@@ -1,19 +1,39 @@
 window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-var start = null;
-var ele = document.getElementById("test");
-var progress = 0;
+// var start = null;
+// var ele = document.getElementById("test");
+// var progress = 0;
 
-function step(timestamp) {
-    progress += 1;
-    ele.style.width = progress + "%";
-    ele.innerHTML=progress + "%";
-    if (progress < 100) {
-        requestAnimationFrame(step);
-    }
+// function step(timestamp) {
+//     progress += 1;
+//     ele.style.width = progress + "%";
+//     ele.innerHTML=progress + "%";
+//     if (progress < 100) {
+//         requestAnimationFrame(step);
+//     }
+// }
+// requestAnimationFrame(step);
+// document.getElementById("run").addEventListener("click", function() {
+//     ele.style.width = "1px";
+//     progress = 0;
+//     requestAnimationFrame(step);
+// }, false);
+
+var dur = 2000;
+var p = 0;
+var startTime = Date.now();
+var start = null;
+var easing = function(p){return p*p};
+
+
+function slide(timestamp){
+	var square = document.getElementById("square");
+	p = (Date.now()-startTime)/dur;
+
+	if(p >= 1){
+		square.style.top = 300 + "px";
+	} else{
+		square.style.top = 100 + p*200 + "px";//ease p*p*200
+		requestAnimationFrame(slide);
+	}
 }
-requestAnimationFrame(step);
-document.getElementById("run").addEventListener("click", function() {
-    ele.style.width = "1px";
-    progress = 0;
-    requestAnimationFrame(step);
-}, false);
+requestAnimationFrame(slide)
